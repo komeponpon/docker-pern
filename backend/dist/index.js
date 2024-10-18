b592b3b0fd8050525926931d57653c3b944a2782
komeponpon <ricemountain1030@gmail.com>
2024-10-18 21:04:27 +0900

    initial commit

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -11,11 +11,4 @@ const port = process.env.PORT || 5001;
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-// Prisma Clientを使用したデータベース操作の例
-app.get('/solar-systems', async (req, res) => {
-    const solarSystems = await prisma.solar_system.findMany();
-    res.json(solarSystems);
-});
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,12 +9,3 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Prisma Clientを使用したデータベース操作の例
-app.get('/solar-systems', async (req, res) => {
-  const solarSystems = await prisma.solar_system.findMany();
-  res.json(solarSystems);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
